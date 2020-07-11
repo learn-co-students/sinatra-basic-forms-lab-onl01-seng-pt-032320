188e63ff44f90e38695ae7bf7a512b0a9c8cd47e
arnie b <arnold.bwaila@gmail.com>
2020-07-11 17:25:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    erb :index
+  end
+  
+  get '/view' do 
+    erb :create_puppy
+  end
+  
+  post '/puppy' do 
+    
+    erb :display_puppy
+  end
 
 end
